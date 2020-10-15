c1366d43a2ae586e957bba237a867ba8cb73cfe7
Fariz Rafiqi <45725951+rafiqprogrammer@users.noreply.github.com>
2020-10-15 08:40:43 +0700

    membenarkan link bootstrap

--- resources/views/web/frontend/layout.blade.php
+++ resources/views/web/frontend/layout.blade.php
@@ -9,7 +9,7 @@
 
   <title>@yield('title')</title>
   <!-- Bootstrap CSS -->
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+  <link href="{{ asset('/public/css/app.css') }}" rel="stylesheet" />
   
   <!-- Custom Styles -->
   @stack('stylesheets')
